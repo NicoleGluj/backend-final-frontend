14c3ddb81615ef4b6269c57a4268e2fd46267d80
NicoleGluj <nicolegluj@gmail.com>
2025-12-08 04:37:56 -0300

    fixed url

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -28,7 +28,7 @@ const Login = () => {
     e.preventDefault()
 
     try {
-      const response = await fetch("${API_URL}/auth/login", {
+      const response = await fetch(`${API_URL}/auth/login`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(formData)
